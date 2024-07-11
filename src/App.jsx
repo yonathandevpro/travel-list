6b5c06ef088979a8a153ae5f0b7b18c3791683cf
yonathandevpro <yonathanber@icloud.com>
2024-07-11 14:44:41 +0300

    Rendered the items list

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Logo from "./Components/Logo";
 import Form from "./Components/Form";
 import PackingList from "./Components/PackingList";
 import Stats from "./Components/Stats";
+
 export default function App() {
   return (
     <div className="app">
