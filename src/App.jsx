ccf94587bf68a76fd08fa889a70b2b9f1719b5d9
yonathandevpro <yonathanber@icloud.com>
2024-07-12 22:45:29 +0300

    Calculating stats functionality

--- src/App.jsx
+++ src/App.jsx
@@ -31,7 +31,7 @@ export default function App() {
         onDeleteItem={handleDeleteItem}
         onToggleItem={handleToggleItem}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
